da3ada5a8a4b24d65fcb1f2374d834a595dd7f42
MusicGindos <rlgindos@gmail.com>
2017-01-15 19:30:24 +0200

    first deploy

---
+++ modules/celebs.py
@@ -0,0 +1,73 @@
+from twitter import *
+import re
+from collections import Counter
+from threading import Thread
+
+consumer_key = "1b7veNvOFSLQobADTLDCkqNpD"
+consumer_secret = "FbbKPoJUOOb7mBd1mHdOatksrNlcH3tJQbkENDmdjm1eayfM8U"
+access_key = "798072331864309765-fcTHr0dBJhIAjWpBaGBC9KqR86nkXN5"
+access_secret = "3FtgkmtDLOVf9qdXZi0Crr8G94GTZQKoZOAgL8I9VHdvh"
+twitter = Twitter(
+    auth=OAuth(access_key, access_secret, consumer_key, consumer_secret))
+
+numberOfThreadFinished = 0
+
+def get_celebs(word, result, index):
+    celebNames = []
+    celebsRepeat = []
+    regex = r"\sis\sa*\s" + word
+    q1 = '/"is a ' + word + '/"'
+    tweetsWithWord = twitter.search.tweets(q=q1, count=100)['statuses']
+    tempUser = {}
+    retweets = 0
+    for tweet in tweetsWithWord:
+        outputObj = {'mentions': tweet['entities']['user_mentions'], 'userName': tweet['user']['screen_name'],
+                     'text': tweet['text'], 'good': {}}
+        retweets += tweet['retweet_count']
+        matches = re.search(regex, outputObj['text'])
+        if matches:
+            firstPart = re.split(regex, outputObj['text'])[0]
+            name = firstPart.split(" ")
+            firstName = name[-1]
+            if firstName and firstName[0].isupper():
+                lastName = firstPart[-2]
+                if lastName and lastName[0].isupper():
+                    outputObj['good'] = firstName + lastName
+                else:
+                    outputObj['good'] = firstName
+            if outputObj['good']:
+                celebNames.append(outputObj['good'])
+    outputObj['retweet_count'] = retweets
+    if celebNames:
+        celebName = Counter(celebNames).most_common(1)[0]
+        if celebName[0] not in celebsRepeat:
+            celebsRepeat.append(celebName[0])
+            users = twitter.users.search(q=celebName[0], count=20)
+            followers_count = 0
+            userName = 'a'
+            image = 'b'
+            for user in users:
+                if user["followers_count"] > followers_count:
+                    userName = user["name"]
+                    followers_count = user["followers_count"]
+                    image = user["profile_image_url"].replace('_normal', '')
+        tempUser["name"] = userName;
+        tempUser["image"] = image
+        tempUser["word"] = word.upper()
+        tempUser["retweet_count"] = retweets
+    result[index] = tempUser
+    global numberOfThreadFinished
+    numberOfThreadFinished += 1
+
+def celebs():
+    threads = [None] * 10
+    results = [None] * 10
+    badWords = ["racist", "fascist", "ugly", "stupid", "liar", "corrupt", "fat", "misogynist", "chauvinist", "idiot"]
+    for i in range(len(threads)):
+        threads[i] = Thread(target=get_celebs, args=(badWords[i], results, i))
+        threads[i].start()
+
+    while True:
+        if numberOfThreadFinished == 10:
+            break
+    return results
---
+++ twitterService.py
@@ -0,0 +1,36 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import os
+from modules import celebs
+
+class S(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print('got GET request: ' + self.path)
+        data = {}
+        if self.path == "/getCelebs":
+            data = celebs.celebs()
+        # elif self.path.startswith("/celeb/"):
+        #     name = self.path.split("/")[2]
+        #     print(self.path.split("/")[2])
+        #     data = data1
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode("utf-8"))
+
+
+def run(server_class=HTTPServer,handler_class=S, port=5000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd... on port ' + str(port))
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(os.environ.get("PORT", 5000)))
+    else:
+        run()
