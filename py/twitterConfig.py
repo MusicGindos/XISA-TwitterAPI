bc1a120888fbd59c8368e2f1df8e5978240ed07a
MusicGindos <rlgindos@gmail.com>
2017-01-16 20:42:04 +0200

    added directories and file reader module

---
+++ py/twitterConfig.py
@@ -0,0 +1,27 @@
+celebs = {
+    "consumer_key": "7ySzkH1tEGHoWPrp8XFxv0WhF",
+   "consumer_secret": "wot5847iNdtg5WljCdeZ8EjTtfqBJMKptzPWNWjCGGMM8CEz5s",
+    "access_key": "798072331864309765-vQIC5LK928Lsoluko39vpMSUocV65fH",
+    "access_secret": "LZl8wYs8EXTOktYtK132J9UkJQXOYF4335OnaESKTQSTY"
+}
+
+celeb = {
+    "consumer_key": "Kyhc8aUt3YpBQ3O6zlXYHr0mv",
+   "consumer_secret": "GJfllBUOYE96Gmnbp2I291iJClkbuiyEIPAGUMyg6IUtRrRPl5",
+    "access_key": "798072331864309765-9Np7nbGBLWZ4ANfFXaDD7iF63FGrfuH",
+    "access_secret": "DmvRNyMv21KEKV4e0Qocnz8Kk6eCVrkfOaEzfsicvvKin"
+}
+
+users = {
+    "consumer_key": "trH6tl4etAuWRUTEC0wxyrl3Z",
+   "consumer_secret": "jQlFhtBKojzMEoMb32GOcDNIb4CCxo4EB0RurQEMWHWTTVmTd3",
+    "access_key": "798072331864309765-G3WpTZQhIZ7dA0v9vSwOAsYjvATWHWa",
+    "access_secret": "wLkDMgAGjYMaP2PRdc6Cuz4dE7kYwZ2YV79JY7eGq1eYV"
+}
+
+user = {
+    "consumer_key": "1b7veNvOFSLQobADTLDCkqNpD",
+   "consumer_secret": "FbbKPoJUOOb7mBd1mHdOatksrNlcH3tJQbkENDmdjm1eayfM8U",
+    "access_key": "798072331864309765-fcTHr0dBJhIAjWpBaGBC9KqR86nkXN5",
+    "access_secret": "3FtgkmtDLOVf9qdXZi0Crr8G94GTZQKoZOAgL8I9VHdvh"
+}
