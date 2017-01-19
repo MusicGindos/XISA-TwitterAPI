5604a3873237db73484387a91f7e9448515ff4c6
MusicGindos <rlgindos@gmail.com>
2017-01-19 08:19:05 +0200

    fixed user

--- modules/user.py
+++ modules/user.py
@@ -118,8 +118,5 @@ def get_user(screen_name):
                     words_with_texts.sort(key=lambda x: x['count'], reverse=True)
                     result["images"] = list(set(images)) #remove duplicates
                     result["words_with_texts"] = words_with_texts[:5]
-                else:
-                    global result
-                    result = {}
             break
     return result
