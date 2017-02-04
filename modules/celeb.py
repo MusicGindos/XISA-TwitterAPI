0970e6b84d846d2f620792da2ec672deb40eaba1
MusicGindos <rlgindos@gmail.com>
2017-02-04 20:05:23 +0200

    Fixed bad words count bug by category

--- modules/celeb.py
+++ modules/celeb.py
@@ -22,7 +22,6 @@ def get_tweets(name, word, result, index):  # get name of celeb and word to sear
     texts_array = []
     for tweet in tweets:
         if tweet['text'] not in texts_array:
-            total_bad_tweets += 1
             texts_array.append(tweet['text'])
             obj = {"tweet": tweet['text'], "tweet_id": tweet["id_str"], "created_time": tweet["created_at"], "name": tweet['user']["name"], "twitter_name": tweet['user']["screen_name"]}
             texts[word]['texts'].append(obj)
@@ -30,6 +29,7 @@ def get_tweets(name, word, result, index):  # get name of celeb and word to sear
     for text in texts[word]['texts']:
         bad_words_count += text["tweet"].count(word)  # count number of bad words in each text
     texts[word]['bad_words_count'] = bad_words_count
+    total_bad_tweets += len(texts[word]["texts"])
     texts[word]['texts'] = texts[word]['texts'][:10]
     if texts[word]['bad_words_count'] is not 0:  # if no bad words -> don't push the dict
         result[index] = texts[word]
